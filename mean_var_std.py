bb55a75b974ee87841b7f2405c3d75aa3543f047
Sean Soon <seansoon1103@gmail.com>
2024-06-11 19:15:56 +0100

    Update mean_var_std.py

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,15 @@
 import numpy as np
 
-def calculate(list):
 
-
-
-
-    return calculations
+def calculate(numlist):
+    if len(numlist) != 9:
+        raise ValueError('List must contain nine numbers.')
+    matrix = np.reshape(numlist, (3, 3))
+    return {
+        'mean': [list(np.mean(matrix, axis=0)), list(np.mean(matrix, axis=1)), np.mean(numlist)],
+        'variance': [list(np.var(matrix, axis=0)), list(np.var(matrix, axis=1)), np.var(numlist)], 
+        'standard deviation': [list(np.std(matrix, axis=0)), list(np.std(matrix, axis=1)), np.std(numlist)],
+        'min': [list(np.min(matrix, axis=0)), list(np.min(matrix, axis=1)), np.min(numlist)],
+        'max': [list(np.max(matrix, axis=0)), list(np.max(matrix, axis=1)), np.max(numlist)],
+        'sum': [list(np.sum(matrix, axis=0)), list(np.sum(matrix, axis=1)), np.sum(numlist)]
+    }
